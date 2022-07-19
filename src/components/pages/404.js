96913f077f4689c2bf0e6c6f233578453fb5cebf
Denys <denys.mazuryk@gmail.com>
2022-07-19 22:02:31 +0300

    Complite Aplication

---
+++ src/components/pages/404.js
@@ -0,0 +1,15 @@
+import ErrorMasenge from "../errorMassege/ErrorMassege";
+import { Link } from "react-router-dom";
+
+
+const Page404 = () => {
+    return (
+        <div>
+            <ErrorMasenge/>
+            <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px'}}>Page doesn't exist</p>
+            <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px'}} to="/">Back to main page</Link>
+        </div>
+    )
+}
+
+export default Page404;
